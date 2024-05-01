0a5791993242ac43f3c7a570be69a80a35250ed2
Belen Agüero <aguerogisela03@gmail.com>
2024-04-30 21:39:52 -0300

    agregue funcionalidad regresar

--- main.js
+++ main.js
@@ -100,6 +100,7 @@ const mostrarDetalleAlummna = (personaje) => {
 
 
             <div class="card-modal">
+                    <a href="javascript:void(o)" class="go_back">Regresar</a>
                 <img class="movie-img" src="${imagen}" alt="Imagen de personajes" />
                 <div class="text-movie-cont">
                 <h1>${nombre}r</h1>
@@ -114,6 +115,103 @@ const mostrarDetalleAlummna = (personaje) => {
                 <button class="delete-button">Eliminar</button>
                 </div>
                 </div>
-            </div>  `;
+
+            </div>  <div class="container-form-editar">
+			<div class="custom-container-editar">
+				<!-- Aquí se agrega el botón "X" -->
+
+				<form id="custom-form-editar" class="hidden">
+					<button class="close-btn" type="button">
+						<svg
+							xmlns="http://www.w3.org/2000/svg"
+							viewBox="0 0 24 24"
+							fill="none"
+							stroke="currentColor"
+							stroke-width="2"
+							stroke-linecap="round"
+							stroke-linejoin="round"
+							class="feather feather-x"
+						>
+							<line x1="18" y1="6" x2="6" y2="18"></line>
+							<line x1="6" y1="6" x2="18" y2="18"></line>
+						</svg>
+					</button>
+					<h3>Edita tu personaje</h3>
+					<h4>Recuerda...</h4>
+					<fieldset>
+						<label for="nombre">Nombre de personaje</label>
+						<input
+							type="text"
+							placeholder="Ej. Meredith Grey"
+							tabindex="1"
+							required
+							autofocus
+							id="personaje-input"
+						/>
+						<label for="especialidad">Especialidad</label>
+						<input
+							type="text"
+							placeholder="Ej. Cirugía general"
+							tabindex="2"
+							required
+							id="especialidad-input"
+						/>
+
+						<label for="temporadas">Temporadas en las que aparece</label>
+						<input
+							type="number"
+							placeholder="Ej. 20"
+							tabindex="3"
+							required
+							id="temporadas-input"
+						/>
+
+						<label for="estado">Estado</label>
+						<input
+							type="text"
+							placeholder="Ej. Vivo"
+							tabindex="4"
+							required
+							id="estado-input"
+						/>
+
+						<label for="imagen">Imagen</label>
+						<input
+							type="url"
+							placeholder="Ej. https://ejemplo.jpg"
+							tabindex="5"
+							required
+							id="imagen-input"
+						/>
+					</fieldset>
+					<fieldset>
+						<label for="descripcion">Descripción</label>
+						<textarea
+							placeholder="Ej. Meredith es la protagonista principal de la serie..."
+							tabindex="6"
+							required
+							id="descripcion-input"
+						></textarea>
+						<input type="submit" name="" id="Editar" />
+						<button
+							type="submit"
+							name="submit"
+							data-submit="...Sending"
+							class="btn-editar-modal"
+						>
+							Editar personaje
+						</button>
+					</fieldset>
+				</form>
+			</div>
+		</div>`;
+		document
+			.querySelector(".go_back")
+			.addEventListener("click", () => getPersonajes(baseUrl));
+		//
 	}, 2000);
 };
+
+//confirmar editar personaje
+const confirmarEditarPersonaje = () => {};
+//asignar funcion editar a botones
